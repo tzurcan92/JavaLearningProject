843fe64543032282483e033019552a97107220e8
sergiu <tzurcan92@gmail.com>
2024-04-21 14:38:50 +0300

    Added examples of var and comments

---
+++ src/main/java/md/tekwillacademy/variablesandcomments/CommentsAndVariables.java
@@ -0,0 +1,34 @@
+package md.tekwillacademy.variablesandcomments;
+
+public class CommentsAndVariables {
+    public static void main(String[] args) {
+        int varInt;
+       boolean varBoolean;/*declararea variabilei*/
+       char varChar;
+       long varLong;
+       short varShort;
+       double varDouble;
+       float varFloat;
+       byte varByte;
+       varInt=2;/*initializarea variabilei*/
+       varBoolean=true;
+       varChar='A';
+       varLong=324235432;
+       varShort=564;
+       varDouble=6.45423524d;
+       varFloat=4.567453324356534f;
+       varByte=45;
+       String objectTypeString;
+       Object objectTypeObject;
+       objectTypeObject = new Object();
+        System.out.println("The value for var int is: "+varInt);
+        System.out.println("The value for var boolean is: "+varBoolean);
+        System.out.println("The value for var char is: "+varChar);
+        System.out.println("The value for var long is: "+varLong);
+        System.out.println("The value for var short is: "+varShort);
+        System.out.println("The value for var double is: "+varDouble);
+        System.out.println("The value for var float is: "+varFloat);
+        System.out.println("The value for var byte is: "+varByte);
+
+    }
+}
