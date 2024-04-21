365c87d66391bfb005f52922af031fda258ba3b7
sergiu <tzurcan92@gmail.com>
2024-04-21 15:49:45 +0300

    Added instructions for solving the comments from reviewers

---
+++ src/main/java/md/tekwillacademy/gitworkflow/GitWorkflow.java
@@ -0,0 +1,38 @@
+package md.tekwillacademy.gitworkflow;
+
+public class GitWorkflow {
+    public static void main(String[] args) {
+        String step1="Run the command git stash, in order to save the changes";
+        String step2="Execute the git checkout master command to switch to the master branch";
+        String step3="Get the latest changes - sync with origin/master, run the command git fetch";
+        String step4="Download the differences locally - git pull";
+        String step5="Create a new branch for the task, git checkout -b branch_name";
+        String step6="Add code changes for implemented task";
+        String step7="Run the git commit command to save the changes in the local memory";
+        String step8 ="Execute git push to upload changes in the cloud";
+        String step9="Go to the GitHyb repository and create a new pull request";
+        String step10="Invite the collaborators to review the changes and get approves";
+        String step11="Resolve the comments if there are any";
+        String step12="Execute the git commit amend and then run a git force push";
+        String step13="Merge the branch to the master";
+        String step14="Delete the created branch as it is already in the master and run git checkout master";
+        String step15="Download the latest version of the origin/master locally";
+        System.out.println(step1);
+        System.out.println(step2);
+        System.out.println(step3);
+        System.out.println(step4);
+        System.out.println(step5);
+        System.out.println(step6);
+        System.out.println(step7);
+        System.out.println(step8);
+        System.out.println(step9);
+        System.out.println(step10);
+        System.out.println(step11);
+        System.out.println(step12);
+        System.out.println(step13);
+        System.out.println(step14);
+        System.out.println(step15);
+
+
+    }
+}
